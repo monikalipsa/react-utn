958d71b7dbd10b1c7b14bdc28467a3fd2133d7f8
monicalipsa <mgp9483@gmail.com>
2022-12-10 14:25:04 -0300

    agrego estilos

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,9 +1,14 @@
+import './Title.css';
+
 
 // Exportamos nuestro componente Title
 // Los componenes son funciones
 export default function Title (){
        return (
-                  <h1>Hola, te hablo desde el componente</h1>
+              //className es el equivalente a class en JSX
+              <h1 className="title">
+                     Hola, te hablo desde el componente
+              </h1>
 
        )
 }
