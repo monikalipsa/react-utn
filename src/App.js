6437cbd10899fee2183f36a62407ce7c28c96e96
monicalipsa <mgp9483@gmail.com>
2022-12-15 20:21:51 -0300

    bucles y propos

--- src/App.js
+++ src/App.js
@@ -5,9 +5,32 @@ import Card from './components/Card';
 import './App.css';
 
 function App() {
+            const numeros =  [45, 78, 12, 787, 341 ];
   return (
     <div className="App">
-      <Title />
+
+           {/* ----FORMA ORIGINAL:
+                     {numeros.map(numero =>  {
+                          return(
+                            <p> El número es: {numero}</p>
+                          );
+                     })} 
+                       el .map requiere un reurn, puedo usar () para que este implicito*/}
+
+             
+            {/* -----IMPLICITO------------- */}
+            
+            {numeros.map(numero=> (
+                <p>El numero es:{numero}</p>
+            ))} 
+
+          
+          {/* Llamamos al componente y le enviamos el parámetro */}
+            <Title
+               titulo = {'Hola, como va'} 
+              subtitulo = {'Este es el subtitulo del Title'}
+             />
+             
       <Card />
       <Mascota />
       <header className="App-header">
--- src/components/Title.js
+++ src/components/Title.js
@@ -3,12 +3,31 @@ import './Title.css';
 
 // Exportamos nuestro componente Title
 // Los componenes son funciones
-export default function Title (){
+export default function Title ({titulo, subtitulo}) {
        return (
-              //className es el equivalente a class en JSX
-              <h1 className="title">
-                     Hola, te hablo desde el componente
-              </h1>
+                  //className es el equivalente a class en JSX
+                  <div>
+                             <h1 className="title">
+                                   {titulo}
+                             </h1>
+                              {/* Pregunto si se recibio un subtitulo
+                                      Si es asi creo el parrafo, sino no */}
+                             {subtitulo &&
+                               <p> {subtitulo} </p>
+                             }
+                {/* OPCION 1:
+                 Pregunto si se recibiò un subtitulo, si es asi lo muestro
+                 sino mustro un texto predeterminado
+                       TERNARY / ONICIONAL TERNARIO */}
+                <p> {subtitulo ? subtitulo : 'predeterminado'} </p>
 
-       )
+                {/* OPCION 2:
+                  Si el subtitulo se envio, lo muestro, sino no hago nada:
+                      {subtitulo &&
+                             <p>{subtitulo}</p>
+                       } */}
+                           
+                 </div>
+            
+        )
 }
